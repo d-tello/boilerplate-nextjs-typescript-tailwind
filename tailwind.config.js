0ba969df6490ced2c4189afc0dfe7f6c9eaffcce
Davide Tello <tello.davide@gmail.com>
2022-06-29 10:02:04 +0200

    Upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
 module.exports = {
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/modules/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './public/index.html',
+  ]
 };
